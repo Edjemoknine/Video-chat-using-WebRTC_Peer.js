59fc472c96cff4365d21df741ecbc6238bcd15e6
moknine <mokninemoknine999@gmail.com>
2024-01-23 18:50:38 +0100

    create video chat  using WebRTC by Peer.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,82 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useEffect, useRef, useState } from "react";
+import Peer from "peerjs";
 
 function App() {
+  const [peerId, setPeerId] = useState();
+  const [CallID, setCallID] = useState("");
+  const remoteVideoStream = useRef();
+  const CurrentVideoStream = useRef();
+  const PeerInstance = useRef();
+
+  useEffect(() => {
+    const peer = new Peer();
+    peer.on("open", (id) => {
+      setPeerId(id);
+    });
+
+    var getUserMedia =
+      navigator.getUserMedia ||
+      navigator.webkitGetUserMedia ||
+      navigator.mozGetUserMedia;
+
+    peer.on("call", function (call) {
+      getUserMedia({ video: true, audio: true }, function (stream) {
+        call.answer(stream);
+
+        CurrentVideoStream.current.srcObject = stream;
+        CurrentVideoStream.current.play();
+
+        call.on("stream", function (remoteStream) {
+          remoteVideoStream.current.srcObject = remoteStream;
+          remoteVideoStream.current.play();
+        });
+      });
+    });
+
+    PeerInstance.current = peer;
+  }, []);
+
+  const CallUser = (RemotePeerId) => {
+    var getUserMedia =
+      navigator.getUserMedia ||
+      navigator.webkitGetUserMedia ||
+      navigator.mozGetUserMedia;
+
+    getUserMedia({ video: true, audio: true }, function (stream) {
+      CurrentVideoStream.current.srcObject = stream;
+      CurrentVideoStream.current.play();
+
+      var call = PeerInstance.current.call(RemotePeerId, stream);
+
+      call.on("stream", function (remoteStream) {
+        remoteVideoStream.current.srcObject = remoteStream;
+        remoteVideoStream.current.play();
+      });
+    });
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>{peerId}</h1>
+      <div>
+        <input
+          type="text"
+          value={CallID}
+          onChange={(e) => setCallID(e.target.value)}
+        />
+        <button onClick={() => CallUser(CallID)}>Call</button>
+      </div>
+      <div
+        style={{
+          display: "grid",
+          gridTemplateColumns: "repeat(auto-fit,minmax(300px,1fr))",
+          gap: "20px",
+        }}
+      >
+        <video autoPlay playsInline ref={remoteVideoStream} />
+        <video autoPlay playsInline ref={CurrentVideoStream} />
+      </div>
     </div>
   );
 }
